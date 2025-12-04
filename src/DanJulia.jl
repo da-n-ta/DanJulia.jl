aa443666d492dd6efe0548ed1b7627d2628effcc
Emma Roume <144689771+emmarme@users.noreply.github.com>
2025-12-04 10:22:41 +0100

    fonction S optimal

--- src/DanJulia.jl
+++ src/DanJulia.jl
@@ -64,4 +64,22 @@ end
 # tracer cette erreur en fct de S
 plot(S_values, errors, xlabel="S", ylabel="Erreur (norme 1)", title="Erreur entre theta_estime et theta_vraie en fonction de S", legend=false)
 
+# Trouver le S optimal qui minimise l'erreur
+function S_optimal(X,y,theta_true)
+    errors = []
+    S_vals = 4.0:0.01:8.0
+    for S in S_vals
+        theta_est = p2(X,y,S)
+        error = norm(theta_est - theta_true, 1)
+        push!(errors, error)
+    end
+    S_opt = S_vals[argmin(errors)]
+    print(S_opt," est la valeur de S qui minimise l'erreur.")
+    print(", avec une erreur de ", minimum(errors))
 end
+
+S_optimal(X,y,theta_true)
+
+
+end
+
