38b1579b992aa651b7abb57f5b5484a020183003
da-n-ta <73463092+da-n-ta@users.noreply.github.com>
2025-12-04 10:46:17 +0100

    Update optim.jl

--- src/optim.jl
+++ src/optim.jl
@@ -1,15 +1,7 @@
 using Convex,SCS
 
 # Generate random problem data
-using Random,LinearAlgebra
-Random.seed!(2)
-n = 10
-m = 20
-sigma = 0.1
-X = randn(m,n)
-theta_true = rand(n)
-xi = randn(m)
-y = X * theta_true + sigma * xi
+
 
 # Define the optimization variable
 function p1(X, y)
@@ -22,8 +14,7 @@ function p1(X, y)
     return theta.value
 end
 
-p1(X, y)
-println("theta_vraie = ", theta_true)
+
 
 # Define another optimization variable but thiss time with constraints
 # la contraintes est que theta>=0 et sum(theta)<=S
@@ -36,9 +27,8 @@ function p2(X, y, S)
     println("theta_estime = ", theta.value)
     return theta.value
 end
-# pour S=3
-p2(X, y, 3.0)
-println("theta_vraie = ", theta_true)
+
+
    
 
 # faire varier S
@@ -70,4 +60,3 @@ function S_optimal(X,y,theta_true)
     print(", avec une erreur de ", minimum(errors))
 end
 
-S_optimal(X,y,theta_true)
